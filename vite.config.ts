0ab778c1a0f50f3c0fdbfc90b99425e8cb16cc48
Harsh Bhardwaj <bhardwajharshmait23@gmail.com>
2024-11-05 20:20:53 +0530

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Investo_Internship',
+  base: 'Invsto_Internship',
   plugins: [react()],
 })
